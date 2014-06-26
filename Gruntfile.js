65b2dd9424f2a62c17c030252b0d52fe97bf85a0
Daiwei Lu <daiwei@spanishdict.com>
2014-06-26 09:50:55 -0400

    Render CSS for retina

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,42 @@
 module.exports = function (grunt)  {
 
   grunt.initConfig({
-
     spriteGenerator: {
-      sprite: {
+
+      regularSprite: {
         src: [
-          'images-sprite/*.png'
+          'images-sprite/regular/*.png'
         ],
         spritePath: 'images-final/sprite.png',
-        stylesheet: 'less',
-        stylesheetPath: 'less/sprite.less'
-      }
-    }
+        stylesheet: 'css', // it can also generate `scss` or `less`
+        stylesheetPath: 'css/sprite.css'
+      },
 
+      retinaSprite: {
+        src: [
+          'images-sprite/retina/*.png'
+        ],
+        spritePath: 'images-final/sprite@2x.png',
+        stylesheet: 'css', // it can also generate `scss` or `less`
+        stylesheetPath: 'css/sprite-retina.css',
+        stylesheetOptions: {
+          pixelRatio: 2,
+          nameMapping: function(name) {
+            var paths = name.split('/');
+            var match = /^(.+)(?:@2x\..+)$/.exec(paths[paths.length - 1]);
+            if (match) {
+              return match[1];
+            } else {
+              throw "Could not match \"@2x\" in image name";
+            }
+          }
+        }
+      }
+
+    }
   });
 
   grunt.loadNpmTasks('node-sprite-generator');
+  grunt.registerTask('default', ['spriteGenerator:regularSprite',
+                                 'spriteGenerator:retinaSprite']);
 };
